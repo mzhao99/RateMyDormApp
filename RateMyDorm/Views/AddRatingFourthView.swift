908b8c9ea579b1111e2677d5464211839e50552c
Manling (Jenny) Zhao <jennyzhao0711@gmail.com>
2023-12-12 14:14:28 -0500

    finished add rating fourth view

--- RateMyDorm/Views/AddRatingFourthView.swift
+++ RateMyDorm/Views/AddRatingFourthView.swift
@@ -17,6 +17,10 @@ struct AddRatingFourthView: View {
     @Binding var photo: Data?
     
     @State private var showFinalView = false
+    @State private var classYears = ["Freshman", "Sophomore", "Junior", "Senior", "Graduate Student"]
+    @State private var selectedClassYears = Array(repeating: false, count: 5)
+    @State private var roomTypes = ["Single", "Double", "Triple", "Quad", "Suite", "Other"]
+    @State private var selectedRoomTypes = Array(repeating: false, count: 6)
     @Environment(\.presentationMode) private var presentationMode: Binding<PresentationMode>
     
     var body: some View {
@@ -26,7 +30,60 @@ struct AddRatingFourthView: View {
                     ProgressView(value: 0.75)
                         .tint(.teal)
                     
+                    Text("Step 3: Class Years & Room Type")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                        .padding(.top, 5)
                     
+                    Group {
+                        Text("What class year(s) did you live here?")
+                            .font(.title)
+                            .bold()
+                            .padding(.bottom, 5)
+                            .padding(.top, 5)
+                        
+                        ForEach(0..<5) { index in
+                            HStack {
+                                Button(action: {
+                                    selectedClassYears[index].toggle()
+                                }) {
+                                    Image(systemName: selectedClassYears[index] ? "checkmark.square.fill" : "square")
+                                        .resizable()
+                                        .frame(width: 25, height: 25)
+                                        .foregroundColor(selectedClassYears[index] ? .teal : .gray)
+                                }
+                                Text(classYears[index])
+                                    .padding(.horizontal, 5)
+                            }
+                            .padding(.bottom, 10)
+                        }
+                    }
+
+                    
+                    Group {
+                        Text("What type of room(s) did you have?")
+                            .font(.title)
+                            .bold()
+                            .padding(.bottom, 5)
+                        
+                        ForEach(0..<6) { index in
+                            HStack {
+                                Button(action: {
+                                    selectedRoomTypes[index].toggle()
+                                }) {
+                                    Image(systemName: selectedRoomTypes[index] ? "checkmark.square.fill" : "square")
+                                        .resizable()
+                                        .frame(width: 25, height: 25)
+                                        .foregroundColor(selectedRoomTypes[index] ? .teal : .gray)
+                                }
+                                Text(roomTypes[index])
+                                    .padding(.horizontal, 5)
+                            }
+                            .padding(.bottom, 10)
+                        }
+                    }
+                    
+                    // bottom buttons
                     HStack {
                         // previous page
                         Button(action: {
@@ -56,10 +113,15 @@ struct AddRatingFourthView: View {
                             }
                         )
                     }
+                    .padding(.top, 5)
                 }
+                // V Stack padding
+                .padding()
+                .padding(.leading, 10)
+                .padding(.trailing, 10)
             }
-            
         }
+        .navigationBarHidden(true)
     }
 }
 
