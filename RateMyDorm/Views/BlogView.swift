88887cb3717c4a7f9fdf0b164ff4e1260fb96283
Manling (Jenny) Zhao <jennyzhao0711@gmail.com>
2023-12-13 00:31:47 -0500

    added safari view to blog posts

--- RateMyDorm/Views/BlogView.swift
+++ RateMyDorm/Views/BlogView.swift
@@ -6,35 +6,36 @@
 //
 
 import SwiftUI
+import SafariServices
 
 struct BlogView: View {
+    @State private var selectedBlogURL: URL?
+    @State private var isSafariViewPresented = false
+    
     private let blogData: [[String: Any]] = [
         [
             "title": "First Blog",
             "author": "John Doe",
             "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-//            "photo": nil
         ],
         [
             "title": "Second Blog",
             "author": "Jane Doe",
             "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-//            "photo": nil
         ],
         [
             "title": "Third Blog",
             "author": "John Doe",
             "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-//            "photo": nil
         ],
         [
             "title": "Fourth Blog",
             "author": "John Doe",
             "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-//            "photo": nil
         ]
     ]
     
+    private let urls: [URL] = [URL(string: "https://www.example.com/blog1")!, URL(string: "https://www.example.com/blog1")!, URL(string: "https://www.example.com/blog1")!, URL(string: "https://www.example.com/blog1")!]
     
     var body: some View {
         NavigationStack {
@@ -46,14 +47,34 @@ struct BlogView: View {
                                      description: .constant(blogData[num]["description"] as! String),
                                      photo: .constant(nil))
                         .padding(.vertical, 5)
+                        .onTapGesture {
+                            selectedBlogURL = urls[num]
+                            isSafariViewPresented = true
+                            
+                        }
                     }
                 }
             }
             .navigationTitle("Blogs")
+            .fullScreenCover(isPresented: $isSafariViewPresented) {
+                SafariView(url: URL(string: "https://google.com")!)
+                    .ignoresSafeArea()
+            }
         }
     }
 }
 
+struct SafariView: UIViewControllerRepresentable {
+    let url: URL
+
+    func makeUIViewController(context: Context) -> UIViewController {
+        return SFSafariViewController(url: url)
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        
+    }
+}
 
 struct BlogView_Previews: PreviewProvider {
     static var previews: some View {
