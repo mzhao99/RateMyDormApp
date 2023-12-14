802717766677787a53d815a030515d5dd7b03cc6
Manling (Jenny) Zhao <jennyzhao0711@gmail.com>
2023-12-13 22:46:29 -0500

    fetch blog data from firestore and siplay

--- RateMyDorm/Views/BlogCardView.swift
+++ RateMyDorm/Views/BlogCardView.swift
@@ -9,21 +9,35 @@ import SwiftUI
 
 struct BlogCardView: View {
     @Binding var title: String
-    @Binding var author: String
     @Binding var description: String
-    @Binding var photo: Data?
+    @Binding var photo: String?
     
     var body: some View {
         VStack(alignment: .leading) {
             // Image Section
-            if let photoData = photo, let uiImage = UIImage(data: photoData) {
-                Image(uiImage: uiImage)
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(height: 170)
-                    .clipped()
-            } else {
-                // Placeholder image for missing image data
+            if let photo = photo, let imageURL = URL(string: photo) {
+                AsyncImage(url: imageURL) { phase in
+                    switch phase {
+                    case .success(let image):
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: UIScreen.main.bounds.width - 40, height: 170)
+                            .clipped()
+                    case .failure(_):
+                        ProgressView()
+                            .frame(height: 170)
+                    case .empty:
+                        ProgressView()
+                            .frame(height: 170)
+                    @unknown default:
+                        ProgressView()
+                            .frame(height: 170)
+                    }
+                }
+            }
+            else {
+                //Placeholder image for missing image data
                 Image("test_image")
                     .resizable()
                     .aspectRatio(contentMode: .fill)
@@ -36,9 +50,6 @@ struct BlogCardView: View {
                 Text(title)
                     .font(.title3)
                     .bold()
-                    .padding(.bottom, 0.05)
-                Text("by \(author)")
-                    .foregroundColor(.secondary)
                     .padding(.bottom, 1)
                 Text(description)
                     .foregroundColor(.secondary)
@@ -56,6 +67,6 @@ struct BlogCardView: View {
 
 struct BlogCardView_Previews: PreviewProvider {
     static var previews: some View {
-        BlogCardView(title: .constant("Blog Title"), author: .constant("Author Name"), description: .constant("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."), photo: .constant(nil))
+        BlogCardView(title: .constant("Blog Title"), description: .constant("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."), photo: .constant("https://www.bu.edu/housing/files/2019/12/18-1773-HUB1-166-1500x624.jpg"))
     }
 }
