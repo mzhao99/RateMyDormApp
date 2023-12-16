66e2616de1a93778432a91368f7333fbd5ab5faf
Manling (Jenny) Zhao <jennyzhao0711@gmail.com>
2023-12-16 03:09:25 -0500

    enabled adding review data to firebase

--- RateMyDorm/Views/AddRatingFinalView.swift
+++ RateMyDorm/Views/AddRatingFinalView.swift
@@ -6,6 +6,9 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseFirestoreSwift
+import FirebaseFirestore
 
 struct AddRatingFinalView: View {
     @Environment(\.managedObjectContext) private var viewContext
@@ -23,6 +26,7 @@ struct AddRatingFinalView: View {
     
     @State private var classYears = ["Freshman", "Sophomore", "Junior", "Senior", "Graduate Student"]
     @State private var roomTypes = ["Single", "Double", "Triple", "Quad", "Suite", "Other"]
+    @State private var addReviewSuccessful = false
     @Environment(\.presentationMode) private var presentationMode: Binding<PresentationMode>
     
     var body: some View {
@@ -156,30 +160,105 @@ struct AddRatingFinalView: View {
                         
                         
                         // next page
-                        NavigationLink(
-                            destination: AddRatingSuccessView(),
-                            label: {
-                                Text("Submit")
-                                    .font(.system(size: 20))
-                                    .foregroundColor(.white)
-                                    .frame(width: 100, height: 50)
-                                    .background(.teal)
-                                    .cornerRadius(30)
-                            }
-                        )
+                        Button(action: {
+                            submitDataToDatabase()
+                        }, label: {
+                            Text("Submit")
+                                .font(.system(size: 20))
+                                .foregroundColor(.white)
+                                .frame(width: 100, height: 50)
+                                .background(.teal)
+                                .cornerRadius(30)
+                        })
+                        
+                        
+//                        NavigationLink(
+//                            destination: AddRatingSuccessView(),
+//                            label: {
+//                                Text("Submit")
+//                                    .font(.system(size: 20))
+//                                    .foregroundColor(.white)
+//                                    .frame(width: 100, height: 50)
+//                                    .background(.teal)
+//                                    .cornerRadius(30)
+//                            }
+//                        )
+//                        .onTapGesture {
+//                            submitDataToDatabase()
+//                        }
                     }
                 }
                 .padding()
                 .padding(.horizontal, 10)
                 .frame(minHeight: geometry.size.height)
+                
+                
+                NavigationLink(
+                    destination: AddRatingSuccessView().navigationBarHidden(true),
+                    isActive: $addReviewSuccessful,
+                    label: { EmptyView() }
+                )
+                .hidden()
             }
         }
         .navigationBarHidden(true)
+    }
+    
+    func submitDataToDatabase() {
+        print("Submitting data to the database")
+        
+        // Ensure that the current user is available
+        guard let user = userViewModel.currentUser else {
+            print("User data not available")
+            return
+        }
+        
+        let base64String = photo?.base64EncodedString()
+        
+        // Create a dictionary with the review data
+        let reviewData: [String: Any] = [
+            "dormName": selectedDorm,
+            "overallRating": calculateOverallRating(),
+            "roomRating": roomRating,
+            "buildingRating": buildingRating,
+            "bathroomRating": bathroomRating,
+            "locationRating": locationRating,
+            "classYears": getSelectedClassYears(),
+            "roomTypes": getSelectedRoomTypes(),
+            "comment": comment,
+            "photo": base64String ?? "",
+            "universityName": universityName,
+            "userId": user.id!,
+            "timeStamp": FieldValue.serverTimestamp()
+        ]
+        
+        // Add the review data to the "reviews" collection in Firestore
+        Firestore.firestore().collection("review").addDocument(data: reviewData) { error in
+            if let error = error {
+                print("Error adding review to Firestore: \(error.localizedDescription)")
+            } else {
+                print("Review added to Firestore successfully")
+                addReviewSuccessful = true
+            }
+        }
+    }
+    
+    func getSelectedClassYears() -> [String] {
+        return zip(selectedClassYears, classYears).filter { $0.0 }.map { $0.1 }
+    }
+    
+    func getSelectedRoomTypes() -> [String] {
+        return zip(selectedRoomTypes, roomTypes).filter { $0.0 }.map { $0.1 }
+    }
+    
+    func calculateOverallRating() -> Double {
+        return (Double)(roomRating + buildingRating + locationRating + bathroomRating) / (Double)(4)
     }
 }
 
 struct AddRatingFinalView_Previews: PreviewProvider {
     static var previews: some View {
-        AddRatingFinalView(selectedDorm: .constant("Dorm 1"), roomRating: .constant(4), buildingRating: .constant(4), bathroomRating: .constant(3), locationRating: .constant(5), comment: .constant("This is a very very very very very very very very very very very very very very long comment"), photo: .constant(UIImage(systemName: "photo")?.jpegData(compressionQuality: 1.0)), selectedClassYears: .constant([true, true, false, false, false]), selectedRoomTypes: .constant([true, false, false, true, false, false]), universityName: .constant("Northeastern University"))
+        let userViewModel = UserViewModel()
+        AddRatingFinalView(selectedDorm: .constant("Dorm 1"), roomRating: .constant(4), buildingRating: .constant(4), bathroomRating: .constant(3), locationRating: .constant(5), comment: .constant("This is a very very very very very very very very very very very very very very long comment"), photo: .constant(UIImage(systemName: "photo")?.jpegData(compressionQuality: 1.0)), selectedClassYears: .constant([true, true, false, false, false]), selectedRoomTypes: .constant([true, false, false, true, false, false]), universityName: .constant("Northeastern University")).environmentObject(userViewModel)
     }
 }
